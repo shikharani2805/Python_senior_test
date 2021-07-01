9eec25862f87a86c84aca79d63498a8b381050a4
Shikha Rani <66298011+shikharani2805@users.noreply.github.com>
2021-07-01 19:33:56 +0530

    Update business.py

--- test_02/business.py
+++ test_02/business.py
@@ -1,8 +1,7 @@
 from bson import ObjectId
 from app import mongo
 from app import RESOLVED_PERMISSIONS
-
-
+from copy import deepcopy
 def get_unpaginated_client_users(departament_id):
     """generates the list of all clients as a drop down
     """
@@ -27,47 +26,43 @@ def get_unpaginated_client_users(departament_id):
     }
     return resp
 
-
 def dereference_client_users_allowed_actions_for_client(client_users_list):
     # Get all client's api_users in one query
     # Map client_user _id: api_user
     # Extract all referenced permissions
     cu_ids = [cu['_id'] for cu in client_users_list]
     api_users = mongo.api_users.find({'client_user': {'$in': cu_ids}})
-
     permission_ids = list()
     cu_api_user_map = dict()
     for au in api_users:
+        au['role_based_permissions'] =list()
         if au.get("user_role"):
             for r in au.get("user_role"):
                 roles = mongo.roles.find_one({'_id': r, 'enabled': True})
                 if roles:
                     permission_ids += roles.get('permission', list())
+                    au['role_based_permissions'] += roles.get('permission', list())
         permission_ids += au.get('permissions', list())
         cu_api_user_map[au.get('client_user')] = au
-
     # Try to get routes _ids resolved and mapped to permissions from global variable
     route_ids = list()
     for pid in permission_ids:
         route_ids += RESOLVED_PERMISSIONS.get(pid, list())
-
     # Query for all relevant routes and remap them route_id: route
     routes = list(mongo.routes.find({'_id': {'$in': route_ids}}))
     route_id_route_map = {r['_id']: r for r in routes}
-
     # Query for all relevant permissions and remap them permission_id: permission
     # Extract all referenced routines
     relevant_permissions = list(mongo.permissions.find({'_id': {'$in': permission_ids}}))
     permission_id_permission_map = dict()
     for p in relevant_permissions:
         permission_id_permission_map[p['_id']] = p
-
     # Go over client_users list and put whole dereferenced permissions, routes, routines structure under allowed actions
-
     dereferenced_permissions = dict()
-    role_permission = list()
+    # role_permission = list()
     for cu in client_users_list:
-        allowed_actions = list()
+        role_permission = []
+        allowed_actions = []
         au = cu_api_user_map.get(cu['_id'])
         if not au:
             cu['error'] = "No API_User wrapper found, can't resolve allowed_actions"
@@ -79,7 +74,7 @@ def dereference_client_users_allowed_actions_for_client(client_users_list):
             for r in roles:
                 permissions += r.get('permission', list())
                 role_permission += r.get('permission', list())
-
+        permissions = set(permissions)
         for pid in permissions:
             if pid in dereferenced_permissions:
                 allowed_actions.append(dereferenced_permissions[pid])
@@ -99,21 +94,26 @@ def dereference_client_users_allowed_actions_for_client(client_users_list):
                 deref_routes.append(route)
                 if 'ui_aliases' in route:
                     p['ui_aliases'].update(route['ui_aliases'])
-
-            p['role_based'] = False
-            if pid in role_permission:
-                p['role_based'] = True
-
-            p['routes'] = deref_routes
-            p['routines'] = []
+           
+            # p['role_based'] = False
             p['ui_aliases'] = list(p['ui_aliases'])
+            p['routines'] = []
+            p['routes'] = deref_routes
             dereferenced_permissions[pid] = p
-
         allowed_actions.sort(key=lambda p: p.get('name', 'z'))
+        allowed_actions = deepcopy(allowed_actions)
+        for a in allowed_actions:
+            if a["_id"] in role_permission:
+                a["role_based"] = True
+            else:
+                a["role_based"] = False
+            
+            # p['routes'] = deref_routes
+            
+            # dereferenced_permissions[pid] = p
+        # allowed_actions.sort(key=lambda p: p.get('name', 'z'))
         cu['allowed_actions'] = allowed_actions
-
     return client_users_list
-
 
 def dereference_client_users_allowed_cost_centers_for_client(cu_list, departament_id):
     ccs = mongo.cost_centers.find({'departament_id': departament_id, 'is_active': True})
@@ -125,7 +125,6 @@ def dereference_client_users_allowed_cost_centers_for_client(cu_list, departamen
             allowed_cost_centers_expanded.append(dict(name=index[cc_id]['name'], _id=index[cc_id]['_id']))
         cu['allowed_cost_centers'] = allowed_cost_centers_expanded
     return cu_list
-
 
 def dereference_client_users_role_for_client(client_user_id):
     role = list()
